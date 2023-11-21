7dd1555003ebbb162d2a470019fe370b0dd47924
wience <wbdelafuente@up.edu.ph>
2023-11-21 17:25:39 +0800

    Update memory.h

--- files/memory.h
+++ files/memory.h
@@ -6,4 +6,8 @@
 #define GROW_CAPACITY(capacity) \
     ((capacity) < 8 ? 8 : (capacity) * 2)
 
+#define GROW_ARRAY(type, pointer, oldCount, newCount)      \
+    (type *)reallocate(pointer, sizeof(type) * (oldCount), \
+                       sizeof(type) * (newCount))
+
 #endif
