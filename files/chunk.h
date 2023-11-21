e7c2abee266484aae80aed41a1aae86d7d523b64
wience <wbdelafuente@up.edu.ph>
2023-11-21 18:40:47 +0800

    Update chunk.h

--- files/chunk.h
+++ files/chunk.h
@@ -2,6 +2,7 @@
 #define npa_chunk_h
 
 #include "common.h"
+#include "value.h"
 
 typedef enum
 {
@@ -13,6 +14,7 @@ typedef struct
     int count;
     int capacity;
     uint8_t *code;
+    ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk *chunk);
