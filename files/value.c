c8ffcd80cfc3a045efa24229bc58459952666fc1
wience <wbdelafuente@up.edu.ph>
2023-11-21 18:37:48 +0800

    Update value.c

--- files/value.c
+++ files/value.c
@@ -9,3 +9,16 @@ void initValueArray(ValueArray *array)
     array->capacity = 0;
     array->count = 0;
 }
+
+void writeValueArray(ValueArray *array, Value value)
+{
+    if (array->capacity < array->count + 1)
+    {
+        int oldCapacity = array->capacity;
+        array->capacity = GROW_CAPACITY(oldCapacity);
+        array->values = GROW_ARRAY(Value, array->values, oldCapacity, array->capacity);
+    }
+
+    array->values[array->count] = value;
+    array->count++;
+}
