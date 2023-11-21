44347289aa55f471473f2f04e48814d8c0fd9d19
wience <wbdelafuente@up.edu.ph>
2023-11-21 18:42:11 +0800

    Update chunk.c

--- files/chunk.c
+++ files/chunk.c
@@ -27,5 +27,6 @@ void writeChunk(Chunk *chunk, uint8_t byte)
 void freeChunk(Chunk *chunk)
 {
     FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+    freeValueArray(&chunk->constants);
     initChunk(chunk);
 }
