6cf4380855e9a02574367a1d20b95ac3d9366bae
wience <wbdelafuente@up.edu.ph>
2023-11-21 18:09:11 +0800

    Update debug.c

--- files/debug.c
+++ files/debug.c
@@ -12,6 +12,12 @@ void disassembleChunk(Chunk *chunk, const char *name)
     }
 }
 
+static int simpleInstruction(const char *name, int offset)
+{
+    printf("%s\n", name);
+    return offset + 1;
+}
+
 int disassembleInstruction(Chunk *chunk, int offset)
 {
     printf("%04d ", offset);
