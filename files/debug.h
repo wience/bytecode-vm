af058bac30c8389c6b4c442fd6081a413b4e024d
wience <wbdelafuente@up.edu.ph>
2023-11-21 18:01:42 +0800

    Create debug.h

---
+++ files/debug.h
@@ -0,0 +1,9 @@
+#ifndef npa_debug_h
+#define npa_debug_h
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk *chunk, const char *name);
+int disassembleInstruction(Chunk *chunk, int offset);
+
+#endif
