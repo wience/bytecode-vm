e9563ff371f4875be678356dba77651278cecb1f
wience <wbdelafuente@up.edu.ph>
2023-11-21 19:54:46 +0800

    Update vm.c

--- files/vm.c
+++ files/vm.c
@@ -11,6 +11,25 @@ void freeVM()
 {
 }
 
+static InterpretResult run()
+{
+#define READ_BYTE() (*vm.ip++)
+
+    for (;;)
+    {
+        uint8_t instruction;
+        switch (instruction = READ_BYTE())
+        {
+        case OP_RETURN:
+        {
+            return INTERPRET_OK;
+        }
+        }
+    }
+
+#undef READ_BYTE
+}
+
 InterpretResult interpret(Chunk *chunk)
 {
     vm.chunk = chunk;
