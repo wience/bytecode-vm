1027aba740cfdaed58a30c15cfcadc0abf7f00c0
wience <wbdelafuente@up.edu.ph>
2023-11-21 17:22:56 +0800

    memory update

--- files/memory.h
+++ files/memory.h
@@ -3,6 +3,7 @@
 
 #include "common.h"
 
-#define GROW_CAPACITY(capacity) \ ((capacity) < 8 ? 8 : (capacity) * 2)
+#define GROW_CAPACITY(capacity) \
+    ((capacity) < 8 ? 8 : (capacity) * 2)
 
 #endif
