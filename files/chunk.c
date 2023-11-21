7bacf36e06b90b3ba886436cac508643f687f28d
wience <wbdelafuente@up.edu.ph>
2023-11-21 19:05:08 +0800

    Update chunk.c

--- files/chunk.c
+++ files/chunk.c
@@ -19,6 +19,7 @@ void writeChunk(Chunk *chunk, uint8_t byte, int line)
         int oldCapacity = chunk->capacity;
         chunk->capacity = GROW_CAPACITY(oldCapacity);
         chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
+        chunk->lines = GROW_ARRAY(int, chunk->lines, oldCapacity, chunk->capacity);
     }
 
     chunk->code[chunk->count] = byte;
