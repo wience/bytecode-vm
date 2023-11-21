e1e8c0dab61e4803adc3fdb8faa32ee93098b656
wience <wbdelafuente@up.edu.ph>
2023-11-21 18:35:18 +0800

    Create value.c

---
+++ files/value.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include "memory.h"
+#include "value.h"
+
+void initValueArray(ValueArray *array)
+{
+    array->values = NULL;
+    array->capacity = 0;
+    array->count = 0;
+}
