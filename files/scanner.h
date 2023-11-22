214c66b69001e4b0b64bd94f7d1e4a427efeccbd
wience <wbdelafuente@up.edu.ph>
2023-11-22 23:45:52 +0800

    Update scanner.h

--- files/scanner.h
+++ files/scanner.h
@@ -1,6 +1,14 @@
 #ifndef npa_scanner_h
 #define npa_scanner_h
 
+typedef struct
+{
+    TokenType type;
+    const char *start;
+    int length;
+    int line;
+} Token;
+
 void initScanner(const char *source);
 Token scanToken();
 
