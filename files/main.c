70c9297840fb3abf9760c4e0d0f1c5d7644968af
wience <wbdelafuente@up.edu.ph>
2023-11-22 23:28:27 +0800

    Update main.c

--- files/main.c
+++ files/main.c
@@ -28,11 +28,24 @@ static char *readFile(const char *path)
 {
     FILE *file = fopen(path, "rb");
 
+    if (file == NULL)
+    {
+        fprintf(stderr, "Could not open file \"%s\".\n", path);
+        exit(74);
+    }
+
     fseek(file, 0L, SEEK_END);
     size_t fileSize = ftell(file);
     rewind(file);
 
     char *buffer = (char *)malloc(fileSize + 1);
+
+    if (buffer == NULL)
+    {
+        fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
+        exit(74);
+    }
+
     size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
     buffer[bytesRead] = '\0';
 
