08a7a5e8375f6f5f90abd3ae57591f774010df3f
wience <wbdelafuente@up.edu.ph>
2023-11-22 23:23:20 +0800

    Update main.c

--- files/main.c
+++ files/main.c
@@ -24,6 +24,22 @@ static void repl()
     }
 }
 
+static char *readFile(const char *path)
+{
+    FILE *file = fopen(path, "rb");
+
+    fseek(file, 0L, SEEK_END);
+    size_t fileSize = ftell(file);
+    rewind(file);
+
+    char *buffer = (char *)malloc(fileSize + 1);
+    size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
+    buffer[bytesRead] = '\0';
+
+    fclose(file);
+    return buffer;
+}
+
 static void runFile(const char *path)
 {
     char *source = readFile(path);
