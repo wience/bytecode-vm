536b596ca2e83344bf485f5b3966f18d89b09cae
wience <wbdelafuente@up.edu.ph>
2023-11-23 00:15:19 +0800

    Update scanner.c

--- files/scanner.c
+++ files/scanner.c
@@ -36,6 +36,13 @@ static char peek()
     return *scanner.current;
 }
 
+static char peekNext()
+{
+    if (isAtEnd())
+        return '\0';
+    return scanner.current[1];
+}
+
 static bool match(char expected)
 {
     if (isAtEnd())
