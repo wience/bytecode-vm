447f87446e036a58f1fcb52ec5aa318f5557df65
wience <wbdelafuente@up.edu.ph>
2023-11-22 23:16:24 +0800

    Update main.c

--- files/main.c
+++ files/main.c
@@ -5,39 +5,22 @@
 
 int main(int argc, const char *argv[])
 {
-
     initVM();
 
-    Chunk chunk;
-    initChunk(&chunk);
+    if (argc == 1)
+    {
+        repl();
+    }
+    else if (argc == 2)
+    {
+        runFile(argv[1]);
+    }
+    else
+    {
+        fprintf(stderr, "Usage: npa [path]\n");
+        exit(64);
+    }
 
-    int constant = addConstant(&chunk, 1.2);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
-
-    // new test
-
-    constant = addConstant(&chunk, 3.4);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
-
-    writeChunk(&chunk, OP_ADD, 123);
-
-    constant = addConstant(&chunk, 5.6);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
-
-    writeChunk(&chunk, OP_DIVIDE, 123);
-    // end new test
-
-    writeChunk(&chunk, OP_NEGATE, 123);
-
-    writeChunk(&chunk, OP_RETURN, 123);
-
-    disassembleChunk(&chunk, "test chunk");
-    interpret(&chunk);
     freeVM();
-    InterpretResult interpret(Chunk * chunk);
-    freeChunk(&chunk);
     return 0;
 }
