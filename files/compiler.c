a6877314130861d9cad942a912d6007be5ab1494
wience <wbdelafuente@up.edu.ph>
2023-11-23 01:41:17 +0800

    Update compiler.c

--- files/compiler.c
+++ files/compiler.c
@@ -7,23 +7,7 @@
 void compile(const char *source, Chunk *chunk)
 {
     initScanner(source);
-
-    int line = -1;
-    for (;;)
-    {
-        Token token = scanToken();
-        if (token.line != line)
-        {
-            printf("%4d ", token.line);
-            line = token.line;
-        }
-        else
-        {
-            printf("   | ");
-        }
-        printf("%2d '%.*s'\n", token.type, token.length, token.start);
-
-        if (token.type == TOKEN_EOF)
-            break;
-    }
+    advance();
+    expression();
+    consume(TOKEN_EOF, "Expect end of expression.");
 }
