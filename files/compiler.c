5dd111fc54ce54284845d3375637d7c66c7566d7
wience <wbdelafuente@up.edu.ph>
2023-11-23 01:45:25 +0800

    Update compiler.c

--- files/compiler.c
+++ files/compiler.c
@@ -4,6 +4,20 @@
 #include "compiler.h"
 #include "scanner.h"
 
+static void advance()
+{
+    parser.previous = parser.current;
+
+    for (;;)
+    {
+        parser.current = scanToken();
+        if (parser.current.type != TOKEN_ERROR)
+            break;
+
+        errorAtCurrent(parser.current.start);
+    }
+}
+
 void compile(const char *source, Chunk *chunk)
 {
     initScanner(source);
