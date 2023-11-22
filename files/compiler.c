1d269eec9fd1a0c2f7d4a804bc9a37a562356adc
wience <wbdelafuente@up.edu.ph>
2023-11-23 01:51:04 +0800

    Update compiler.c

--- files/compiler.c
+++ files/compiler.c
@@ -12,6 +12,26 @@ typedef struct
 
 Parser parser;
 
+static void errorAt(Token *token, const char *message)
+{
+    fprintf(stderr, "[line *d] Error", token->line);
+
+    if (token->type == TOKEN_EOF)
+    {
+        fprintf(stderr, " at end");
+    }
+    else if (token->type == TOKEN_ERROR)
+    {
+        // nah
+    }
+    else
+    {
+        fprintf(stderr, " at '%.s'", token->length, token->start);
+    }
+    fprintf(stderr, ": %s\n", message);
+    parser.hadError = true;
+}
+
 static void error(const char *message)
 {
     errorAt(&parser.previous, message);
