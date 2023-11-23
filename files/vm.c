93eb023d208acf3841629794796d2ff883bb312f
wience <wbdelafuente@up.edu.ph>
2023-11-23 22:43:13 +0800

    wtf

--- files/vm.c
+++ files/vm.c
@@ -70,6 +70,9 @@ static void concatenate()
     char *chars = ALLOCATE(char, length + 1);
     memcpy(chars, a->chars, a->length);
     memcpy(chars + a->length, b->chars, b->length);
+    chars[length] = '\0';
+
+    ObjString *result = takeString(chars, length);
     push(OBJ_VAL(result));
 }
 
