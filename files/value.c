92712409199aec9450dc51f8c165e5f06e039926
wience <wbdelafuente@up.edu.ph>
2023-11-23 22:20:49 +0800

    Update value.c

--- files/value.c
+++ files/value.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "memory.h"
 #include "value.h"
@@ -61,6 +62,12 @@ bool valuesEqual(Value a, Value b)
         return true;
     case VAL_NUMBER:
         return AS_NUMBER(a) == AS_NUMBER(b);
+    case VAL_OBJ:
+    {
+        ObjString *aString = AS_STRING(a);
+        ObjString *bString = AS_STRING(b);
+        return aString->length == bString->length && memcmp(aString->chars, bString->chars, aString->length) == 0;
+    }
     default:
         return false; // unreachable since all values are accounted
     }
