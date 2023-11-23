fa61c325794fa616a552b5b86c0283de86fcd8db
wience <wbdelafuente@up.edu.ph>
2023-11-23 18:19:59 +0800

    Update value.h

--- files/value.h
+++ files/value.h
@@ -6,7 +6,7 @@
 typedef enum
 {
     VAL_BOOL,
-    VAL_NUL,
+    VAL_NIL,
     VAL_NUMBER,
 } ValueType;
 
@@ -30,8 +30,6 @@ typedef struct
 #define BOOL_VAL(value) ((Value){VAL_BOOL, {.boolean = value}})
 #define NIL_VAL ((Value){VAL_NIL, {.number = 0}})
 #define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
-
-typedef double Value;
 
 typedef struct
 {
