a03d4ae644f1bbe87826b656e154e12bbaf3215e
wience <wbdelafuente@up.edu.ph>
2023-11-23 23:01:08 +0800

    a

--- files/memory.h
+++ files/memory.h
@@ -6,6 +6,8 @@
 #define ALLOCATE(type, count) \
     (type *)reallocate(NULL, 0, sizeof(type) * (count))
 
+#define FREE(type, pointer) reallocate(pointer, sizeof(type), 0)
+
 #define GROW_CAPACITY(capacity) \
     ((capacity) < 8 ? 8 : (capacity) * 2)
 
