778f229e72806bea5ce6709194ed4e6640a4ee70
wience <wbdelafuente@up.edu.ph>
2023-11-23 18:50:31 +0800

    Update vm.c

--- files/vm.c
+++ files/vm.c
@@ -109,6 +109,13 @@ static InterpretResult run()
         case OP_FALSE:
             push(BOOL_VAL(false));
             break;
+        case OP_EQUAL:
+        {
+            Value b = pop();
+            Value a = pop();
+            push(BOOL_VAL(valuesEqual(a, b)));
+            break;
+        }
         case OP_ADD:
             BINARY_OP(NUMBER_VAL, +);
             break;
