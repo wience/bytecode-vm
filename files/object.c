899eeaf438c8aa2aa9593c176af3344bdabcbc6a
wience <wbdelafuente@up.edu.ph>
2023-11-23 22:04:17 +0800

    Update object.c

--- files/object.c
+++ files/object.c
@@ -9,6 +9,13 @@
 #define ALLOCATE_OBJ(type, objectType) \
     (type *)allocateObject(sizeof(type), objectType)
 
+static Obj *allocateObject(size_t size, ObjType type)
+{
+    Obj *object = (Obj *)reallocate(NULL, 0, size);
+    object->type = type;
+    return object
+}
+
 static ObjString *allocateString(char *chars, int length)
 {
     ObjString *string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
