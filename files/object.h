487e7db806f4104121de1ee8234cda89d113ca0c
wience <wbdelafuente@up.edu.ph>
2023-11-23 21:46:45 +0800

    Update object.h

--- files/object.h
+++ files/object.h
@@ -7,6 +7,9 @@
 #define OBJ_TYPE(value) (AS_OBJ(value)->type)
 
 #define IS_STRING(value) isObjType(value, OBJ_STRING)
+
+#define AS_STRING(value) ((ObjString *)AS_OBJ(value))
+#define AS_CSTRING(value) (((ObjString *)AS_OBJ(value))->chars)
 typedef enum
 {
     OBJ_STRING,
@@ -23,5 +26,10 @@ struct ObjString
     int length;
     char *chars;
 };
+
+static inline bool isObjType(Value value, ObjType type)
+{
+    return IS_OBJ(value) && AS_OBJ(value)->type == type;
+}
 
 #endif
