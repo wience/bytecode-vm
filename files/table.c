0ce1578158dd0d6ef442a962d1e6a544bf35384e
wience <wbdelafuente@up.edu.ph>
2023-11-24 17:09:48 +0800

    Update table.c

--- files/table.c
+++ files/table.c
@@ -21,6 +21,11 @@ void freeTable(Table *table)
 
 bool tableSet(Table *table, ObjString *key, Value value)
 {
+    if (table->count + 1 > table->capacity * TABLE_MAX_LOAD)
+    {
+        int capacity = GROW_CAPACITY(table->capacity);
+        adjustCapacity(table, capacity)
+    }
     Entry *entry = findEntry(table->entries, table->capacity, key);
     bool isNewKey = entry->key == NULL;
     if (isNewKey)
