b125b8d088407c950333736b77902acab0c95a45
wience <wbdelafuente@up.edu.ph>
2023-11-24 20:42:02 +0800

    Update table.h

--- files/table.h
+++ files/table.h
@@ -21,6 +21,7 @@ void initTable(Table *table);
 void freeTable(Table *table);
 bool tableSet(Table *table, ObjString *key, Value value);
 bool tableGet(Table *table, ObjString *key, Value *value);
+bool tableDelete(Table *table, ObjString *key);
 void tableAddAll(Table *from, Table *to);
 
 #endif
