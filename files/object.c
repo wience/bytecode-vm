f458c9d91dd898c7078a3dfea69d740f9bf1885f
wience <wbdelafuente@up.edu.ph>
2023-11-24 21:04:29 +0800

    Update object.c

--- files/object.c
+++ files/object.c
@@ -25,6 +25,7 @@ static ObjString *allocateString(char *chars, int length, uint32_t hash)
     string->length = length;
     string->chars = chars;
     string->hash = hash;
+    tableSet(&vm.strings, string, NIL_VAL);
     return string;
 }
 
