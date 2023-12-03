6a65321a710d50feb9b51c990d98f9ffdfec4818
wience <wbdelafuente@up.edu.ph>
2023-12-03 08:48:16 +0800

    Update vm.c

--- files/vm.c
+++ files/vm.c
@@ -182,6 +182,12 @@ static InterpretResult run()
             }
             push(NUMBER_VAL(-AS_NUMBER(pop())));
             break;
+        case OP_PRINT:
+        {
+            printValue(pop());
+            printf("\n");
+            break;
+        }
         case OP_RETURN:
         {
             printValue(pop());
