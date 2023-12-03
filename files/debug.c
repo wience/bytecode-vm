4eb54047901b016cf63d5e659bfa6017776c17c2
wience <wbdelafuente@up.edu.ph>
2023-12-03 15:00:09 +0800

    Update debug.c

--- files/debug.c
+++ files/debug.c
@@ -28,6 +28,13 @@ static int simpleInstruction(const char *name, int offset)
     return offset + 1;
 }
 
+static int byteInstruction(const char *name, Chunk *chunk, int offset)
+{
+    uint8_t slot = chunk->code[offset + 1];
+    printf("%16s %4d\n", name, slot);
+    return offset + 2;
+}
+
 int disassembleInstruction(Chunk *chunk, int offset)
 {
     printf("%04d ", offset);
