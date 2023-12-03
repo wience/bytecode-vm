7a7741252e23aea6b6cceb1ee6fac6622dc2894c
wience <wbdelafuente@up.edu.ph>
2023-12-03 14:14:38 +0800

    Update compiler.c

--- files/compiler.c
+++ files/compiler.c
@@ -414,6 +414,21 @@ static void declareVariable()
         return;
 
     Token *name = &parser.previous;
+
+    for (int i = current->localCount - 1; i >= 0; i--)
+    {
+        Local *local = &current->locals[i];
+        if (local->depth != -1 && local->depth < current->scopeDepth)
+        {
+            break;
+        }
+
+        if (identifiersEqual(name, &local->name))
+        {
+            error("Already a variable with this name in this scope.");
+        }
+    }
+
     addLocal(*name);
 }
 
