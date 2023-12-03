597abcb77430e9f3f4d2942de73ab51e18a34c28
wience <wbdelafuente@up.edu.ph>
2023-12-03 14:56:05 +0800

    Update vm.c

--- files/vm.c
+++ files/vm.c
@@ -142,6 +142,12 @@ static InterpretResult run()
             push(vm.stack[slot]);
             break;
         }
+        case OP_SET_LOCAL:
+        {
+            uint8_t slot = READ_BYTE();
+            vm.stack[slot] = peek(0);
+            break;
+        }
         case OP_GET_GLOBAL:
         {
             ObjString *name = READ_STRING();
