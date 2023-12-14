8b104a6047c9e1a7fc175f48e93468c89c12094d
wience <wbdelafuente@up.edu.ph>
2023-12-14 23:41:11 +0800

    Update debug.c

--- files/debug.c
+++ files/debug.c
@@ -114,6 +114,16 @@ int disassembleInstruction(Chunk *chunk, int offset)
         printf("%s-16 %4d ", "OP_CLOSURE", constant);
         printValue(chunk->constants.values[constant]);
         printf("\n");
+
+        ObjFunction *function = AS_FUNCTION(chunk->constants.values[constant]);
+        for (int j = 0; j < function->upvalueCount; j++)
+        {
+            int isLocal = chunk->code[offset++];
+            int index = chunk->code[offset++];
+            printf("%04d      |                     %s %d\n",
+                   offset - 2, isLocal ? "local" : "upvalue", index);
+        }
+
         return offset;
     }
     case OP_RETURN:
