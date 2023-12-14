fbd3fe7885625fb7b9544e1fec3461d300c72f04
wience <wbdelafuente@up.edu.ph>
2023-12-14 19:15:02 +0800

    Update object.c

--- files/object.c
+++ files/object.c
@@ -102,6 +102,9 @@ void printObject(Value value)
     case OBJ_FUNCTION:
         printFunction(AS_FUNCTION(value));
         break;
+    case OBJ_NATIVE:
+        printf("<native fn>");
+        break;
     case OBJ_STRING:
         printf("%s", AS_CSTRING(value));
         break;
