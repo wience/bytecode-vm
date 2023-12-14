7fc69eefa78e5dfc8572ac27ab1c713c841a70f0
wience <wbdelafuente@up.edu.ph>
2023-12-14 16:24:53 +0800

    Update compiler.c

--- files/compiler.c
+++ files/compiler.c
@@ -603,6 +603,20 @@ static void expressionStatement()
     emitByte(OP_POP);
 }
 
+static void forStatement()
+{
+    consume(TOKEN_LEFT_PAREN, "Expect '(' after 'for'.");
+    consume(TOKEN_SEMICOLON, "Expect ';'.");
+
+    int loopStart = currentChunk()->count;
+
+    consume(TOKEN_SEMICOLON, "Expect ';'.");
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clauses.");
+
+    statement();
+    emitLoop(loopStart);
+}
+
 static void ifStatement()
 {
     consume(TOKEN_LEFT_PAREN, "Expected '(' after 'if'.");
