fb99e41a6636e8e834f2ea237ee5abf5e4a8aa98
wience <wbdelafuente@up.edu.ph>
2023-12-14 23:07:51 +0800

    Update object.h

--- files/object.h
+++ files/object.h
@@ -7,6 +7,7 @@
 
 #define OBJ_TYPE(value) (AS_OBJ(value)->type)
 
+#define IS_CLOSURE(value) isObjType(value, OBJ_CLOSURE)
 #define IS_FUNCTION(value) isObjType(value, OBJ_FUNCTION)
 #define IS_NATIVE(value) isObjType(value, OBJ_NATIVE)
 #define IS_STRING(value) isObjType(value, OBJ_STRING)
