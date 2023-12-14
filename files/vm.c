f086efa76f006dc0f178cc2e998da60c597603c9
wience <wbdelafuente@up.edu.ph>
2023-12-14 23:59:07 +0800

    Update vm.c

--- files/vm.c
+++ files/vm.c
@@ -268,17 +268,23 @@ static InterpretResult run()
             }
             break;
         }
+        case OP_GET_UPVALUE:
+        {
+            uint8_t slot = READ_BYTE();
+            push(*frame->closure->upvalues[slot]->location);
+            break;
+        }
+        case OP_SET_UPVALUE:
+        {
+            uint8_t slot = READ_BYTE();
+            *frame->closure->upvalues[slot]->location = peek(0);
+            break;
+        }
         case OP_EQUAL:
         {
             Value b = pop();
             Value a = pop();
             push(BOOL_VAL(valuesEqual(a, b)));
-            break;
-        }
-        case OP_GET_UPVALUE:
-        {
-            uint8_t slot = READ_BYTE();
-            push(*frame->closure->upvalues[slot]->location);
             break;
         }
         case OP_GREATER:
