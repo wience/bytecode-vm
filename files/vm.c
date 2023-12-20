929a761bcb83cf12d0fe65e949b7e0020d42c285
wience <wbdelafuente@up.edu.ph>
2023-12-20 23:49:42 +0800

    Update vm.c

--- files/vm.c
+++ files/vm.c
@@ -1,4 +1,3 @@
-#include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
@@ -11,15 +10,15 @@
 #include "memory.h"
 #include "vm.h"
 
-VM vm;
+VM vm; // Global VM instance.
 
-void printBytecode(Chunk *chunk);
-
+// Native function to get the current time.
 static Value clockNative(int argCount, Value *args)
 {
     return NUMBER_VAL((double)clock() / CLOCKS_PER_SEC);
 }
 
+// Native function to read input from the user.
 static Value inputNative(int argCount, Value *args)
 {
     char inputBuffer[1024];
@@ -34,8 +33,10 @@ static Value inputNative(int argCount, Value *args)
         return NIL_VAL;
     }
 
+    // Remove newline character.
     inputBuffer[strcspn(inputBuffer, "\n")] = 0;
 
+    // Try to convert to integer.
     char *end;
     long intVal = strtol(inputBuffer, &end, 10);
     if (*end == '\0')
@@ -43,15 +44,18 @@ static Value inputNative(int argCount, Value *args)
         return NUMBER_VAL((double)intVal);
     }
 
+    // Try to convert to floating point.
     double floatVal = strtod(inputBuffer, &end);
     if (*end == '\0')
     {
         return NUMBER_VAL(floatVal);
     }
 
+    // Return as string if not a number.
     return OBJ_VAL(copyString(inputBuffer, strlen(inputBuffer)));
 }
 
+// Resets the VM stack to its initial state.
 static void resetStack()
 {
     vm.stackTop = vm.stack;
@@ -59,14 +63,17 @@ static void resetStack()
     vm.openUpvalues = NULL;
 }
 
+// Prints a runtime error and its location.
 static void runtimeError(const char *format, ...)
 {
+    // Print formatted error message.
     va_list args;
     va_start(args, format);
     vfprintf(stderr, format, args);
     va_end(args);
     fputs("\n", stderr);
 
+    // Print stack trace.
     for (int i = vm.frameCount - 1; i >= 0; i--)
     {
         CallFrame *frame = &vm.frames[i];
@@ -82,9 +89,11 @@ static void runtimeError(const char *format, ...)
             fprintf(stderr, "%s()\n", function->name->chars);
         }
     }
+
     resetStack();
 }
 
+// Defines a native function in the VM.
 static void defineNative(const char *name, NativeFn function)
 {
     push(OBJ_VAL(copyString(name, (int)strlen(name))));
@@ -94,25 +103,29 @@ static void defineNative(const char *name, NativeFn function)
     pop();
 }
 
+// Initializes the VM.
 void initVM()
 {
     resetStack();
     vm.objects = NULL;
 
+    // Initialize memory management fields.
     vm.bytesAllocated = 0;
     vm.nextGC = 1024 * 1024;
-
     vm.grayCount = 0;
     vm.grayCapacity = 0;
     vm.grayStack = NULL;
 
+    // Initialize global variables and string intern table.
     initTable(&vm.globals);
     initTable(&vm.strings);
 
+    // Define native functions.
     defineNative("clock", clockNative);
     defineNative("input", inputNative);
 }
 
+// Frees resources used by the VM.
 void freeVM()
 {
     freeTable(&vm.globals);
@@ -120,22 +133,30 @@ void freeVM()
     freeObjects();
 }
 
+// Pushes a value onto the VM stack.
 void push(Value value)
 {
     *vm.stackTop = value;
     vm.stackTop++;
 }
 
+// Pops a value from the VM stack.
 Value pop()
 {
     vm.stackTop--;
     return *vm.stackTop;
 }
 
+// Peeks at a value on the stack without popping it.
 static Value peek(int distance)
 {
     return vm.stackTop[-1 - distance];
 }
+
+// More VM functions follow, including `call`, `callValue`, `captureUpvalue`, `closeUpvalues`, `isFalsey`, `concatenate`, and `run`.
+// These functions handle function calls, upvalue management, logical operations, string concatenation, and the main bytecode execution loop.
+// The `run` function is particularly important, as it reads and executes bytecode instructions in a loop, effectively running the VM.
+// `interpret` is the entry point for executing a script, compiling the source code and initiating its execution.
 
 static bool call(ObjClosure *closure, int argCount)
 {
